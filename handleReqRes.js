2b3facadb0465c05af6048fbfd758975aaa143d0
shakib04 <51458806+shakib04@users.noreply.github.com>
2022-10-08 22:33:37 +0600

    code refactored into a separate branch

---
+++ handleReqRes.js
@@ -0,0 +1,30 @@
+const handler = {}
+handler.handleReqRes = (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  //console.log(req);
+  console.log(parsedUrl.pathname);
+  req.method = 'POST';
+  console.log(req.method);
+  const queryStringObject = parsedUrl.query;
+  console.log(queryStringObject);
+  const headers = req.headers;
+  headers.title = 'test title';
+  //console.log(headers);
+
+  //string decoder
+  // const stringDecoder = new StringDecoder('UTF-8');
+  // let realDate = '';
+  // req.on('data', (buffer) => {
+  //   realData += decoder.write(buffer);
+  // });
+
+  // req.on('end', () => {
+  //   realData += decoder.end();
+  //   console.log(decoder);
+  //   res.end('Hello World after save');
+  // });
+
+  res.end('Hello World after save');
+};
+
+module.exports = handler;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log(`Hello, Node.js v${process.versions.node}!`);
 
 const http = require('http');
 const url = require('url');
-const { StringDecoder } = require('string_decoder');
+const { handleReqRes } = require('./helpers/handleReqRes');
 
 const app = {};
 
@@ -19,32 +19,6 @@ app.createServer = () => {
   });
 };
 
-app.handleReqRes = (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  //console.log(req);
-  console.log(parsedUrl.pathname);
-  req.method = 'POST';
-  console.log(req.method);
-  const queryStringObject = parsedUrl.query;
-  console.log(queryStringObject);
-  const headers = req.headers;
-  headers.title = 'test title';
-  //console.log(headers);
-
-  //string decoder
-  // const stringDecoder = new StringDecoder('UTF-8');
-  // let realDate = '';
-  // req.on('data', (buffer) => {
-  //   realData += decoder.write(buffer);
-  // });
-
-  // req.on('end', () => {
-  //   realData += decoder.end();
-  //   console.log(decoder);
-  //   res.end('Hello World after save');
-  // });
-
-  res.end('Hello World after save');
-};
+app.handleReqRes = handleReqRes;
 
 app.createServer();
